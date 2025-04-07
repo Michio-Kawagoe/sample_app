0a65f3c55191e91bad0fdfe5b695c7bd5384d7ac
Michio-Kawagoe <201214880+Michio-Kawagoe@users.noreply.github.com>
2025-04-07 13:49:32 +0000

    [in the middle] 3-7 ビューから

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -31,6 +31,12 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
 
+  def destory
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destory                   # データ（レコード）を削除
+    redirect_to '/lists'           # 投稿一覧画面へリダイレクト 
+  end
+
   private
   # ストロングパラメータ
   def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'list/:id' => 'list#destroy', as: 'destory_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
